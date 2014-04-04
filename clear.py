5bcdedb2859736eb03fc36573f0bfb8aaffc1bb3
CodingYue <this.isssssyy@gmail.cp>
2014-04-04 21:14:37 +0800

    Fix clear.py

--- clear.py
+++ clear.py
@@ -3,14 +3,30 @@ import os
 import re
 import pdb
 
-def dfs(curdir) :
-	print curdir
+def dfs(curdir, remove = False) :
 	for name in os.listdir(curdir) :
+		if name == '.git' : continue
 		fullname = curdir + '/' + name
 		if re.match(r'.*dSYM', name) != None :
-			#print curdir + name
-			#os.system('rm -rf %s' %(curdir + name))
+			if remove :
+				os.system('rm -rf %s' % fullname)
+				print remove
+			else:
+				print 1, name
+			continue
+		if re.match(r'^(?:.*?\.(?:cpp|py|tex|java|md|gitignore))|LICENSE$', name) == None and os.path.isdir(fullname) == False :
+			if remove :
+				os.system('rm -rf %s' % fullname)
+				print remove
+			else:
+				print 2, name
 			continue
 		if os.path.isdir(fullname) :
-			dfs(fullname)
+			dfs(fullname, remove)
+
 dfs(os.getcwd())
+sure = raw_input('Sure to remove files mentioned above? yes or no : ')
+remove = False
+if sure == 'yes' :
+	remove = True
+dfs(os.getcwd(), True)
