481903e089cbc376fa42802cf92f148be6ea93c2
CodingYue <this.isssssyy@gmail.com>
2014-08-10 22:26:10 +0800

    summary changed

--- acm-summary/make.sh
+++ acm-summary/make.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-cd tex/
-OUT=$1
-FILE=$1.tex
-xelatex $FILE 
-rm -f $OUT.log $OUT.aux
-mv $OUT.pdf ../pdf/
-open ../pdf/$OUT.pdf
+cnt=0
+for var in $*
+do
+	cnt=$((cnt+1))
+done
+for var in $*
+do
+	OUT=${var%.*}
+	FILE=$OUT.tex
+	echo $OUT
+	if [ $cnt -eq 1 ] ; then
+		xelatex $FILE
+	else
+		xelatex $FILE > /dev/null
+	fi
+done
+rm -f *.log *.aux
+rm -f template.pdf
+mv *.pdf pdf/
+if [ $cnt -eq 1 ] ; then
+	OUT=${1%.*}
+	X=${OUT##*/}
+	open pdf/$X.pdf
+fi
